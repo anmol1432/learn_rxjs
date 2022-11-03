2f4d819ab7abc3fc03a608fd3fe6af213f4b6f14
anmol <anmolbuttar143205gmail.com>
2022-11-03 09:27:17 +0530

    browserify js for browser

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const { Observable,fromEvent } = require("rxjs");
+const { map } = require("rxjs/operators");
+
+const users = {
+  data: [
+    {
+      status: "active",
+      name: "anmol",
+    },
+    {
+      status: "inactive",
+      name: "anmol2",
+    },
+    {
+      status: "active",
+      name: "love",
+    },
+  ],
+};
+
+const observable = new Observable((subscriber) => {
+  // an observable to emit av avlue of 10
+  subscriber.next(users);
+  // subscriber.next(103);
+  // subscriber.next(106);
+}).pipe(map((value) => {
+    // console.log("1:) got data from observable \n",value.data)
+    /* [
+    {
+        staus: "active",
+        name: "anmol",
+    }
+    ]*/
+    return value.data
+}), map((value)=>{
+    // console.log("2:) got data from first map operator \n",value[0])
+    // { staus: 'active', name: 'anmol' }
+    return value.filter((item)=> item.status=="active" ); 
+ })); 
+
+const observer = {
+  next: (value) => {
+    console.log("the value is " , value);
+  },
+  error: (err) => {
+    console.log("the err is " + err);
+  },
+  complete: () => {
+    console.log("the observer got a  complete notfification");
+  },
+};
+
+observable.subscribe(observer);
+
+// grab button reference
+const button = document.getElementById('myButton');
+
+// create an observable of button clicks
+const myObservable = fromEvent(button, 'click');
+
+// for now, let's just log the event on each click
+const subscription = myObservable.subscribe(event => console.log(event));
